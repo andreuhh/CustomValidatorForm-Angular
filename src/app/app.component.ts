93ae5c05dfdd78d1bcbed5652accc80f394d04af
Andrea Guffi <andreaguffi92@gmail.com>
2021-03-21 15:48:44 +0100

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 import { AuthService } from './auth/auth.service';
 
 @Component({
@@ -7,15 +8,11 @@ import { AuthService } from './auth/auth.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  signedin = false;
+  signedin$: BehaviorSubject<boolean>;
 
   constructor(
     private authService: AuthService
-  ) { }
+  ) { this.signedin$ = this.authService.signedin$; }
 
-  ngOnInit() {
-    this.authService.signedin$.subscribe((signedin) => {
-      this.signedin = signedin;
-    })
-  }
+
 }
