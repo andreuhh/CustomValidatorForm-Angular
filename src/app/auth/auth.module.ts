8d40519171d4aed92298f6a551fd8e2907a8c544
Andrea Guffi <andreaguffi92@gmail.com>
2021-03-21 09:05:13 +0100

    shared inputs

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -5,14 +5,15 @@ import { ReactiveFormsModule } from '@angular/forms'
 import { AuthRoutingModule } from './auth-routing.module';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
-
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [SigninComponent, SignupComponent],
   imports: [
     CommonModule,
     AuthRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    SharedModule
   ]
 })
 export class AuthModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 import { InputComponent } from './input/input.component';
 
 
@@ -7,7 +8,9 @@ import { InputComponent } from './input/input.component';
 @NgModule({
   declarations: [InputComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    ReactiveFormsModule
+  ],
+  exports: [InputComponent]
 })
 export class SharedModule { }
